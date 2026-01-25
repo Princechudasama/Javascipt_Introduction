67093ee06fbfa4fc40d1121579d5e84f9287e2b7
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-25 10:49:52 +0000

    make notes of data types

--- 01_Basics/02_Data_types.js
+++ 01_Basics/02_Data_types.js
@@ -26,5 +26,48 @@ console.log(typeof null); //it shows an object class
 console.log(typeof undefined);//it shows an undefined class
 console.log(typeof(NaN)); //it shows an number class
 
+// primitive directly allocate content first
+// 7 types : string , number, boolean, null, undefined, symbol,bigInt
+let string1 = "Prince";
+const num = 1123;
+let isLoggedIn = true;
+let temperature = null;
+let varUndefined;
+let bigNumber = 8128762006n
+
+let sym = Symbol('123');
+let sym1 = Symbol('123');
+//console.log(sym == sym1); //it is a false although it is same 
+
+//Reference -- call refenrce only allocate space and address first
+//Array, Objects,Functions
+let a = ["Prince","jayraj","het"];
+
+let obj1 = {
+    name : ["Prince","Jayraj","het"],
+    age: [10,19,25]
+}
+console.table(obj1);
+
+let b = function a () {
+    console.log("hello world");
+    
+}
+
+console.log(typeof(NaN)); // null - object
+                          // array - object but function
+                          // object - object but function
+                          // undefined - undefined
+                          // nan - number
+                          // function - function    but object function
+
+
+
+
+
+
+
+
+
 
 
