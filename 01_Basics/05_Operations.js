57e8a85e654fbf56c72db7b034720ec1f77e06f7
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-25 06:47:47 +0000

    numbering all files and add new file operatin.ts

---
+++ 01_Basics/05_Operations.js
@@ -0,0 +1,41 @@
+// operations
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "Prince ";
+let str2 = "Chudasama";
+let fullname  = str1 + str2;
+console.log(fullname);
+
+console.log("1"+2); // first comes that should be a consider whole number
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let gamecounter = 100;
+gamecounter++; //here ++ operation is always perform but below it shows an precedency
+console.log(gamecounter);
+
+
+let x = 3;
+let y = x++;
+console.log(`x:${x} , y:${y}`);
+
+let a = 3;
+let b = ++a;
+console.log(`a:${a} , b:${b}`);
+
+
+
+ 
+
+
