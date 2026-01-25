75470e95dadf66576567602827443a29c7b339cd
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-25 07:08:26 +0000

    learn comparison

---
+++ 01_Basics/06_comparison.js
@@ -0,0 +1,24 @@
+// comparison operators
+
+// console.log(2 < 5);
+// console.log(2 <= 5);
+// console.log(2 > 5);
+// console.log(2 <= 5);
+// console.log(2 == 5);
+// console.log(2 != 5);
+
+//not compatible data types comarison it converts automatically compatible datatypes
+console.log("2" == 2);
+console.log("02" != 2);
+
+console.log(null > 0);
+console.log(null == 0); // it is shows not predictable output so we have to take care of compatible data types
+console.log(null >= 0); // comparison and == operator work differently and comparison null converts into 0.
+
+console.log("2" == 2); //it check with converting
+console.log("2" === 2); //it check strict without converting
+
+
+
+
+
