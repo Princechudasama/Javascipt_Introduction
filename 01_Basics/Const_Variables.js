8a1c66d24a15be5b542dfb0a884f856732953fd1
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-25 04:31:04 +0000

    add how to work variables

---
+++ 01_Basics/Const_Variables.js
@@ -0,0 +1,32 @@
+console.log("This is the file for Const and variables");
+
+const name = "Prince"; 
+const surname = "Chudasama";
+// name = "shital";  it is not possible const are not modifiyable
+
+console.log(name);
+console.table([name,surname]);
+
+let age = 18;
+age = 20; //it is changable 
+
+console.log(age);
+
+var age1 = 18; // it is as equal to let but it is not use beacuse it is not block scoped..
+console.log(age1);
+
+age2 = 56;
+console.log(age2);
+
+let age3;
+console.log(age3);
+
+
+
+
+
+
+
+
+
+
