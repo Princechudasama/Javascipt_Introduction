f2a2c5e10a06570b79ee800c831bc3e2c5e018a0
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-25 05:12:14 +0000

    add data of datatype

---
+++ 01_Basics/Data_types.js
@@ -0,0 +1,28 @@
+// const { StrictMode } = require("react")
+
+// use StrictMode; //"use strict" in JavaScript turns on strict mode, which makes JavaScript more secure, predictable, and error-friendly.
+
+console.log("This is the file for data types");
+
+// for any documentation mdm And tc39.es
+
+let first_name = "Prince"
+let age = 19
+marks = 100
+let number;
+
+// number = 2 to power of 53
+// bigInt
+// string = ""
+//boolean = True or False
+// null = not a num or big num or standalone value
+// undefined =  untill not assign any value
+// symbol = unique
+// object = it is represent via whole row of any table and just like it
+
+// use of typeof
+console.log(typeof "");
+console.log(typeof null); //it shows an object class
+console.log(typeof undefined);//it shows an undefined class
+
+
