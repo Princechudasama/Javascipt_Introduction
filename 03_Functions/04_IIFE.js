0e0909d054189756678f551b85342946330c006c
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-29 18:01:12 +0000

    add 04_IIFE.js

---
+++ 03_Functions/04_IIFE.js
@@ -0,0 +1,11 @@
+//Immediately Invokes Function expression
+//used to prevent global scopes pollution... ()()
+ (function f1 () {//named IIFY
+    console.log("Db connected");
+ })();//here you dont need to say explicitely that function call
+        //you have to ; use for  separate 2 IIFY function
+
+ ( (name) => (console.log(`the name is ${name}`)//Unnamed IIFY
+) )("Prince");//here you can give arguments ...
+
+
