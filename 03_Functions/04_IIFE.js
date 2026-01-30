8f41d0b40a8b22a1999b787aca304f40079fbc1e
Princekumar Chudasama <chudasamaprince123@gmail.com>
2026-01-30 04:10:33 +0000

    create 04_IIRE.js

--- 03_Functions/04_IIFE.js
+++ 03_Functions/04_IIFE.js
@@ -1,11 +1,21 @@
 //Immediately Invokes Function expression
 //used to prevent global scopes pollution... ()()
+
  (function f1 () {//named IIFY
     console.log("Db connected");
  })();//here you dont need to say explicitely that function call
-        //you have to ; use for  separate 2 IIFY function
+//you have to ; use for  separate 2 IIFY function
 
  ( (name) => (console.log(`the name is ${name}`)//Unnamed IIFY
 ) )("Prince");//here you can give arguments ...
+
+
+(function name () {
+   console.log("Prince")
+})();//it is valid
+
+(() => {
+   console.log("Prince1")
+})();//it is also valid
 
 
